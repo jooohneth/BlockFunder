c3da6dfe53202a08a0cf1d76609f8dfb0a73727f
John <jooohn.eth@gmail.com>
2023-02-11 13:21:00 -0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,40 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  mode: 'jit',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      display: ['Open Sans', 'sans-serif'],
-      body: ['Open Sans', 'sans-serif'],
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
     },
     extend: {
+      colors: {
+        "primary-red": "#C93D4B",
+        "primary-white": "#EFEAE8",
+        "primary-black": "#1F2023",
+      },
       screens: {
-        mf: '990px',
+        mf: "990px",
       },
       keyframes: {
-        'slide-in': {
-          '0%': {
-            '-webkit-transform': 'translateX(120%)',
-            transform: 'translateX(120%)',
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
           },
-          '100%': {
-            '-webkit-transform': 'translateX(0%)',
-            transform: 'translateX(0%)',
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
           },
         },
       },
       animation: {
-        'slide-in': 'slide-in 0.5s ease-out',
+        "slide-in": "slide-in 0.5s ease-out",
       },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
